75abbf5c2f449d48eef531e1fa4696820189b3d1
Ayrat <poohxx84@gmail.com>
2022-01-05 07:36:26 +0300

    first commit

---
+++ app/src/main/java/com/poohxx/earnapp/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.poohxx.earnapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.poohxx.earnapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var interAd: InterstitialAd? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        initAdMob()
+        (application as AppMainState).showAdIfAvailable(this){
+            Toast.makeText(this,"startApp",Toast.LENGTH_LONG).show()
+        }
+        binding.button.setOnClickListener{
+            showInterAd()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.adView.resume()
+        loadInterAd()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.adView.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.adView.destroy()
+    }
+
+    private fun initAdMob() {
+        MobileAds.initialize(this)
+        val adRequest = AdRequest.Builder().build()
+        binding.adView.loadAd(adRequest)
+    }
+
+    private fun loadInterAd() {
+        val adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-3940256099942544/1033173712",
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(p0: LoadAdError) {
+                    interAd = null
+                }
+
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    interAd = ad
+                }
+            })
+
+    }
+
+    private fun showInterAd() {
+        if (interAd != null) {
+            interAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                override fun onAdDismissedFullScreenContent() {
+                    showContent()
+                    interAd = null
+                    loadInterAd()
+                }
+
+                override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                    showContent()
+                    interAd = null
+                    loadInterAd()
+                }
+
+                override fun onAdShowedFullScreenContent() {
+
+                    interAd = null
+                    loadInterAd()
+                }
+            }
+            interAd?.show(this)
+        } else {
+            showContent()
+        }
+    }
+
+    private fun showContent() {
+        Toast.makeText(this, "content content", Toast.LENGTH_SHORT).show()
+    }
+}
